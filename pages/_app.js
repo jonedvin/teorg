5a779a55717778b7efe10d19705c5ca2599b5dac
Jon Edvin Lona <jone.lona@gmail.com>
2024-06-12 11:57:02 +0900

    Fixed minor issues

--- pages/_app.js
+++ pages/_app.js
@@ -8,22 +8,26 @@ function MyApp({ Component, pageProps }) {
 
   // Execute liff.init() when the app is initialized
   useEffect(() => {
-    console.log("start liff.init()...");
-    liff
-      .init({ liffId: process.env.LIFF_ID })
-      .then(() => {
-        console.log("liff.init() done");
-        setLiffObject(liff);
-      })
-      .catch((error) => {
-        console.log(`liff.init() failed: ${error}`);
-        if (!process.env.liffId) {
-          console.info(
-            "LIFF Starter: Please make sure that you provided `LIFF_ID` as an environmental variable."
-          );
-        }
-        setLiffError(error.toString());
-      });
+    if (typeof window !== 'undefined') {
+
+      // Initialize LIFF
+      console.log("start liff.init()...");
+      liff
+        .init({ liffId: process.env.LIFF_ID })
+        .then(() => {
+          console.log("liff.init() done");
+          setLiffObject(liff);
+        })
+        .catch((error) => {
+          console.log(`liff.init() failed: ${error}`);
+          if (!process.env.liffId) {
+            console.info(
+              "LIFF Starter: Please make sure that you provided `LIFF_ID` as an environmental variable."
+            );
+          }
+          setLiffError(error.toString());
+        });
+    }
   }, []);
 
   // Provide `liff` object and `liffError` object
