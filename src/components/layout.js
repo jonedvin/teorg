d209a1add56478a9f8631361f4e61a8b2f628a40
Jon Edvin Lona <jone.lona@gmail.com>
2024-08-23 21:41:32 +0200

    Changed tØrg to button routing to home

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,21 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 
-const Layout = ({ children }) => (
-  <>
-    <Head>
-      <title>tØrg</title>
-    </Head>
-    <div className="layout_div">
-      <h1 className="torg">tØrg</h1>
-      {children}
-    </div>
-  </>
-);
+
+const Layout = ({ children }) => {
+  const router = useRouter();
+
+  return (
+    <>
+      <Head>
+        <title>tØrg</title>
+      </Head>
+      <div className="layout_div">
+        <button className="torg_button" onClick={() => router.push(`/`)}>tØrg</button>
+        {children}
+      </div>
+    </>
+  );
+}
 
 export default Layout;
