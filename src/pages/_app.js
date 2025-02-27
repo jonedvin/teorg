8ce4ffa7806c7a6ad0d7d0f1b3fd130661577217
Jon Edvin Lona <jone.lona@gmail.com>
2025-02-27 13:13:03 +0100

    Trying to remove LINE login to allow demo on Vercel

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -11,7 +11,7 @@ export default function MyApp({ Component, pageProps }) {
   useEffect(() => {
     (async () => {
       await initializeLiff();
-      forceLogin();
+      // forceLogin();
     })();
   }, []);
 
